6ee1abedc0b0bf1f5e248dd66fa841e2e8345c44
clamascabaleiro <clamascabaleiro@nautilus15.danielcastelao.org>
2018-10-31 10:37:29 +0100

    Ejercicio Terminado

--- Boletin6_5/src/boletin6_5/NumeroMayor.java
+++ Boletin6_5/src/boletin6_5/NumeroMayor.java
@@ -17,15 +17,12 @@ public class NumeroMayor {
         if (num1>num2){
             JOptionPane.showMessageDialog(null, "El mayor es : " + num1);
         }
-        else if (num3>num1){
-            JOptionPane.showMessageDialog(null, "El mayor es : " + num3);
+        else if (num2>num3){
+            JOptionPane.showMessageDialog(null, "El mayor es : " + num2);
             
         }
         else if (num3>num2){
             JOptionPane.showMessageDialog(null, "El mayor es : " + num3);
-        }
-        else {
-            JOptionPane.showMessageDialog(null, "El mayor es : " + num2);
         }
         
     }
